0977b0380d31a9358044f1222176f1329977d2dd
ebrahim <ebrahimmosadegh@gmail.com>
2025-01-18 12:12:44 +0330

    add and config react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,31 @@
 import Home from "./pages/home/Home";
+import Users from "./pages/users/Users";
+import Products from "./pages/products/Products";
+import { 
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link
+ } from "react-router-dom";
 
 function App() {
 
-  return (
-    <div><Home /></div>
-  )
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: 
+        <Home />
+    },
+    {
+      path: "users",
+      element: <Users />
+    },
+    {
+      path: "products",
+      element: <Products />
+    },
+  ]);
+  return <RouterProvider router={router} />;
 }
 
 export default App
