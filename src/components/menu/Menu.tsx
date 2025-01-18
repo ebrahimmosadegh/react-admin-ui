6b6d03e52e55eb534346bbe2fc4fcd05a397e283
ebrahim <ebrahimmosadegh@gmail.com>
2025-01-18 19:35:39 +0330

    add data.ts

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -6,9 +6,24 @@ const Menu = () => {
     <div className='menu'>
       <div className="item">
         <span className="title">MAIN</span>
-        <Link to="/">
+        <Link to="/" className="listItem">
           <img src="/home.svg" alt="" />
           <span className="listItemTitle">Home</span>
+        </Link>
+        <Link to="/" className="listItem">
+          <img src="/profile.svg" alt="" />
+          <span className="listItemTitle">Profile</span>
+        </Link>
+      </div>
+      <div className="item">
+        <span className="title">MAIN</span>
+        <Link to="/" className="listItem">
+          <img src="/home.svg" alt="" />
+          <span className="listItemTitle">Home</span>
+        </Link>
+        <Link to="/" className="listItem">
+          <img src="/profile.svg" alt="" />
+          <span className="listItemTitle">Profile</span>
         </Link>
       </div>
     </div>
