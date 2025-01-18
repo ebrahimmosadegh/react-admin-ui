b78f67692b3f92c7362e4ba0ce3c5bbc35e406ce
ebrahim <ebrahimmosadegh@gmail.com>
2025-01-18 12:36:40 +0330

    create login page

--- src/App.tsx
+++ src/App.tsx
@@ -4,26 +4,56 @@ import Products from "./pages/products/Products";
 import { 
   createBrowserRouter,
   RouterProvider,
-  Route,
-  Link
+  Outlet
  } from "react-router-dom";
+import Navbar from "./components/navbar/Navbar";
+import Footer from "./components/footer/Footer";
+import Menu from "./components/menu/Menu";
+import Login from "./pages/login/Login";
 
 function App() {
+
+  const Layout = () => {
+    return(
+      <div className="main">
+        <Navbar />
+        <div className="container">
+          <div className="menuContainer">
+            <Menu />
+          </div>
+          <div className="contentContainer">
+            <Outlet />
+          </div>
+        </div>
+        <Footer />
+      </div>
+    )
+  }
 
   const router = createBrowserRouter([
     {
       path: "/",
       element: 
-        <Home />
+        <Layout />,
+      children:[
+        {
+          path: "/",
+          element:<Home />
+        },
+        {
+          path:"/users",
+          element:<Users />
+        },
+        {
+          path:"/products",
+          element:<Products />
+        }
+      ]  
     },
     {
-      path: "users",
-      element: <Users />
-    },
-    {
-      path: "products",
-      element: <Products />
-    },
+      path:"/login",
+      element:<Login />
+    }
   ]);
   return <RouterProvider router={router} />;
 }
