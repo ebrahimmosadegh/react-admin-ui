d16eacee509d687cead58682a8231cfc13721f00
ebrahim <ebrahimmosadegh@gmail.com>
2025-01-20 22:21:28 +0330

    create Area Chart Box

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -4,6 +4,7 @@ import ChartBox from '../../components/chartBox/ChartBox';
 import { barChartBoxRevenue, barChartBoxVisit, chartBoxConversion, chartBoxProduct, chartBoxRevenue, chartBoxUser } from '../../data';
 import BarChartBox from '../../components/barChartBox/BarChartBox';
 import PieChartBox from '../../components/pieChartBox/PieChartBox';
+import BigChartBox from '../../components/bigChartBox/BigChartBox';
 
 const Home = () => {
   return (
@@ -14,7 +15,7 @@ const Home = () => {
       <div className="box box4"><PieChartBox /></div>
       <div className="box box5"><ChartBox {...chartBoxRevenue}/></div>
       <div className="box box6"><ChartBox {...chartBoxConversion}/></div>
-      <div className="box box7">Box7</div>
+      <div className="box box7"><BigChartBox /></div>
       <div className="box box8"><BarChartBox {...barChartBoxRevenue}/></div>
       <div className="box box9"><BarChartBox {...barChartBoxVisit}/></div>
     </div>
