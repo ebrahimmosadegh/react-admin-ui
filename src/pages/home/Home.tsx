e805dc6a005628f397ef17fba202be1b0e9417b6
ebrahim <ebrahimmosadegh@gmail.com>
2025-01-20 21:46:32 +0330

    create pie chart box

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -3,6 +3,7 @@ import './home.scss';
 import ChartBox from '../../components/chartBox/ChartBox';
 import { barChartBoxRevenue, barChartBoxVisit, chartBoxConversion, chartBoxProduct, chartBoxRevenue, chartBoxUser } from '../../data';
 import BarChartBox from '../../components/barChartBox/BarChartBox';
+import PieChartBox from '../../components/pieChartBox/PieChartBox';
 
 const Home = () => {
   return (
@@ -10,7 +11,7 @@ const Home = () => {
       <div className="box box1"><TopBox /></div>
       <div className="box box2"><ChartBox {...chartBoxUser}/></div>
       <div className="box box3"><ChartBox {...chartBoxProduct}/></div>
-      <div className="box box4">Box4</div>
+      <div className="box box4"><PieChartBox /></div>
       <div className="box box5"><ChartBox {...chartBoxRevenue}/></div>
       <div className="box box6"><ChartBox {...chartBoxConversion}/></div>
       <div className="box box7">Box7</div>
