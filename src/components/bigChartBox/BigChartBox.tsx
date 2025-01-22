ad8583da7eabbe249df6a172e8411bc15f214d81
ebrahim <ebrahimmosadegh@gmail.com>
2025-01-22 16:36:17 +0330

    create Data Table for users

--- src/components/bigChartBox/BigChartBox.tsx
+++ src/components/bigChartBox/BigChartBox.tsx
@@ -10,43 +10,43 @@ type Data = {
 
 const data:Data[] = [
     {
-      name: 'Page A',
+      name: 'Sun',
       books: 4000,
       clothes: 2400,
       electronic: 2400,
     },
     {
-      name: 'Page B',
+      name: 'Mon',
       books: 3000,
       clothes: 1398,
       electronic: 2210,
     },
     {
-      name: 'Page C',
+      name: 'Tue',
       books: 2000,
       clothes: 9800,
       electronic: 2290,
     },
     {
-      name: 'Page D',
+      name: 'Wed',
       books: 2780,
       clothes: 3908,
       electronic: 2000,
     },
     {
-      name: 'Page E',
+      name: 'Thu',
       books: 1890,
       clothes: 4800,
       electronic: 2181,
     },
     {
-      name: 'Page F',
+      name: 'Fri',
       books: 2390,
       clothes: 3800,
       electronic: 2500,
     },
     {
-      name: 'Page G',
+      name: 'Sat',
       books: 3490,
       clothes: 4300,
       electronic: 2100,
@@ -68,7 +68,7 @@ const BigChartBox = () => {
             bottom: 0,
           }}
         >
-          <CartesianGrid strokeDasharray="3 3" />
+          {/* <CartesianGrid strokeDasharray="3 3" /> */}
           <XAxis dataKey="name" />
           <YAxis />
           <Tooltip />
